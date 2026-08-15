60a3541102871da936d04d16e5bf3d442248082a
Hasibull <hasibul.sbn@gmail.com>
2026-08-15 22:48:19 +0600

    removing some common code

--- include/common.h
+++ include/common.h
@@ -3,16 +3,6 @@
 
 #include <stdint.h>
 
-/* -------------------------------------------------------------
- * Xtensa LX106 Special Register Numbers (accessed via RSR/WSR)
- * ------------------------------------------------------------- */
-#define SR_EPC1     177
-#define SR_EPS1     193
-#define SR_EXCSAVE1 209
-#define SR_SAR      3
-#define SR_PS       230
-#define SR_VECBASE  231
-#define SR_INTENABLE 228
-#define SR_INTERRUPT 226
+
 
 #endif // COMMON_H
